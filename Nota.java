3dda17f0cdd5dc544e51817e1cc96f77fa6ada31
thascript <89532022+thascript@users.noreply.github.com>
2022-11-18 22:21:54 -0300

    primeiro commit

---
+++ Nota.java
@@ -0,0 +1,34 @@
+package projeto;
+
+public class Nota {
+  private float ab1;
+  private float ab2;
+
+
+  public Nota(float ab1, float ab2){
+    this.ab1 = ab1;
+    this.ab2 = ab2;
+  }
+
+
+  public float getAb1() {
+    return ab1;
+  }
+
+
+  public void setAb1(float ab1) {
+    this.ab1 = ab1;
+  }
+
+
+  public float getAb2() {
+    return ab2;
+  }
+
+
+  public void setAb2(float ab2) {
+    this.ab2 = ab2;
+  }
+
+  
+}
